c16104ed50c6843af01e7347535fa1f257fcf63b
Vastiansen <vastiansen@gmail.com>
2024-06-25 12:53:50 +0700

    change: port on run server script

--- run_server.sh
+++ run_server.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Define the name of the executable or script to run
-EXECUTABLE="your_go_executable"
+EXECUTABLE="alpha-echo"
 
 # Function to find and kill the running server process
 kill_server() {
-  # Find the PID of the running server
-  PID=$(lsof -t -i:YOUR_SERVER_PORT)
+  PID=$(lsof -t -i:49153)
 
   if [ -n "$PID" ]; then
     echo "Killing existing server process with PID: $PID"
@@ -19,13 +17,10 @@ kill_server() {
 # Function to start the server
 start_server() {
   echo "Starting the server..."
-  # Adjust this line to use the correct command to run your server
   go run . &
-  # Disown the process so it doesn't terminate when the script ends
   disown
 }
 
-# Main script logic
 kill_server
 start_server
 
