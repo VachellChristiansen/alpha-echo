1751675e59adeae8738f15d4b1dd44b638eb8b2a
Vastiansen <vastiansen@gmail.com>
2024-07-20 22:08:06 +0700

    change: model naming broke

--- models/opus_model.go.go
+++ models/opus_model.go.go
@@ -6,34 +6,34 @@ import (
 	"gorm.io/gorm"
 )
 
-type OpusCategory struct {
+type Category struct {
 	gorm.Model
 	Name      string `gorm:"size:255"`
 	Priority  int    `gorm:"default:0"`
 	Status    int    `gorm:"default:0"`
-	Tasks     []OpusTask
+	Tasks     []Task
 	RegularID uint
 }
 
-type OpusTask struct {
+type Task struct {
 	gorm.Model
-	Title         string     `gorm:"size:255;not null"`
-	Details       string     `gorm:"type:text"`
-	Notes         string     `gorm:"type:text"`
-	Priority      int        `gorm:"default:0"`
-	Inset         int        `gorm:"default:1"`
-	DoneAt        time.Time  `gorm:"type:timestamp"`
-	StartDate     time.Time  `gorm:"type:timestamp"`
-	EndDate       time.Time  `gorm:"type:timestamp"`
-	Status        int        `gorm:"default:0"`
-	ParentTask    *OpusTask  `gorm:"foreignKey:ParentID"`
-	ChildrenTasks []OpusTask `gorm:"foreignKey:ParentID"`
+	Title         string    `gorm:"size:255;not null"`
+	Details       string    `gorm:"type:text"`
+	Notes         string    `gorm:"type:text"`
+	Priority      int       `gorm:"default:0"`
+	Inset         int       `gorm:"default:1"`
+	DoneAt        time.Time `gorm:"type:timestamp"`
+	StartDate     time.Time `gorm:"type:timestamp"`
+	EndDate       time.Time `gorm:"type:timestamp"`
+	Status        int       `gorm:"default:0"`
+	ParentTask    *Task     `gorm:"foreignKey:ParentID"`
+	ChildrenTasks []Task    `gorm:"foreignKey:ParentID"`
 	CategoryID    uint
 	ParentID      *uint
-	TaskGoals     []OpusTaskGoal `gorm:"foreignKey:TaskID"`
+	TaskGoals     []TaskGoal `gorm:"foreignKey:TaskID"`
 }
 
-type OpusTaskGoal struct {
+type TaskGoal struct {
 	gorm.Model
 	GoalText  string    `gorm:"type:text"`
 	DoneAt    time.Time `gorm:"type:timestamp"`
--- models/vacuus_model.go
+++ models/vacuus_model.go
@@ -1,1 +1,9 @@
 package models
+
+import "gorm.io/gorm"
+
+type VacuusAnimation struct {
+	gorm.Model
+	Name     string `gorm:"size:255"`
+	Category string `gorm:"size:255"`
+}
